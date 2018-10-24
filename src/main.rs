57ba08c692db3efc5d43cf084a5c9daabfdfdb59
Ryan Chan <rchan@pcopen.net>
2018-10-24 16:56:12 -0700

    adding juniper graphql integration

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,45 @@
 extern crate actix;
 extern crate actix_web;
-extern crate futures;
-extern crate dotenv;
+// extern crate futures;
+// extern crate dotenv;
+extern crate juniper;
 
-use std::env;
+// use std::env;
 use actix_web::{
     App, 
-    AsyncResponder,
-    Body,
-    client,
+    // AsyncResponder,
+    // Body,
+    // client,
     Error, 
-    HttpMessage,
+    // HttpMessage,
     HttpRequest, 
-    HttpResponse, 
+    // HttpResponse, 
+    fs,
+    middleware,
     server, 
-    fs
 };
 // use actix_web::http::StatusCode;
-use futures::{Future, Stream};
-use dotenv::dotenv;
+use fs::NamedFile;
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
+// use futures::{Future, Stream};
+// use dotenv::dotenv;
 
-fn dev_proxy(_req: &HttpRequest) -> Box<Future<Item = HttpResponse, Error = Error>> {
-    let js_proxy = "http://localhost:8082/";
-    client::ClientRequest::get(js_proxy)
-        .finish().expect("failed to fetch js_proxy")
-        .send()
-        .map_err(Error::from)
-        .and_then(|resp| {
-            Ok(HttpResponse::Ok()
-                .body(Body::Streaming(Box::new(resp.payload().from_err()))))
-            
-        })
-        .responder()
-}
-
-fn dev_js_proxy(_req: &HttpRequest) -> Box<Future<Item = HttpResponse, Error = Error>> {
-    let js_proxy = "http://localhost:8082/app.js";
-    client::ClientRequest::get(js_proxy)
-        .finish().expect("failed to fetch js_proxy")
-        .send()
-        .map_err(Error::from)
-        .and_then(|resp| {
-            Ok(HttpResponse::Ok()
-                .body(Body::Streaming(Box::new(resp.payload().from_err()))))
-            
-        })
-        .responder()
+pub fn index(_req: &HttpRequest) -> Result<NamedFile, Error> {
+    Ok(NamedFile::open("ui/dist/index.html")?)
 }
 
 fn main() {
-    dotenv().ok();
     let host = "127.0.0.1:3000";
-    let is_dev = env::var("ENV").expect("failed to read ENV") == "development";
+    // dotenv().ok();
     let sys = actix::System::new("upquark");
     let _addr = server::new(move || {
-            let app = if is_dev {
-                App::new()
-                    .handler("/img", fs::StaticFiles::new("ui/dist/img").expect("fail to handle static images"))
-                    .handler("/js", fs::StaticFiles::new("ui/dist/js").expect("fail to handle static js"))
-                    .handler("/css", fs::StaticFiles::new("ui/dist/css").expect("fail to handle static css"))
-                    .resource("/app.js", |r| r.f(dev_js_proxy))
-                    .resource("/", |r| r.f(dev_proxy))
-            } else {
-                App::new()
-                    .handler("/img", fs::StaticFiles::new("ui/dist/img").expect("fail to handle static images"))
-                    .handler("/js", fs::StaticFiles::new("ui/dist/js").expect("fail to handle static js"))
-                    .handler("/css", fs::StaticFiles::new("ui/dist/css").expect("fail to handle static css"))
-                    .handler("/", fs::StaticFiles::new("ui/dist").expect("fail to handle static files").index_file("index.html"))
-            };
-            app
+        App::new()
+            .resource("/", |r| r.f(index))
+            .handler("/img", fs::StaticFiles::new("ui/dist/img").expect("fail to handle static images"))
+            .handler("/js", fs::StaticFiles::new("ui/dist/js").expect("fail to handle static js"))
+            .handler("/css", fs::StaticFiles::new("ui/dist/css").expect("fail to handle static css"))
+            .handler("/", fs::StaticFiles::new("ui/dist").expect("fail to handle static files"))
         })
     .bind(host).expect(&format!("could not bind to {}", host))
     .start();
